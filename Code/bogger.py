1c53a134c35b86946379c8f05ea08e882282ce72
BuildTools <unconfigured@null.spigotmc.org>
2021-09-10 10:23:50 +0200

    Start

---
+++ Code/bogger.py
@@ -0,0 +1,13 @@
+#############
+# My Tokens #
+#############
+
+T_INT    = 'VolleYoghurt'
+T_FLOAT  = 'HalfvolleYoghurt'
+T_PLUS   = 'meer'
+T_MINUS  = 'minder'
+T_MUL    = 'keer'
+T_DIV    = 'delen'
+T_LPAREN = '\/->'
+T_RPAREN = '/\->'
+
